65734c586b9a4294d52aee9064361ad531c83c0a
shirel-chaulker <111571651+shirel-chaulker@users.noreply.github.com>
2022-09-22 13:26:06 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,24 @@
+md shirell
+cd shirell
+dir >run.txt
+dir
+cd \
+md test123
+cd test123
+md AAA
+md BBB
+md CCC
+cd BBB
+md DDD
+cd..
+cd CCC
+dir >list.txt
+ dir c:/windows>list.txt
+cd..
+cd ..
+del test123
+cd test123
+cd ..
+help del
+del /Q /S test123
+rmdir /s test123
